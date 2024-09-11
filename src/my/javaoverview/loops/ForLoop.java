e9d79b3d484d08837d5558e6987ecbbeac7860c2
Wilson dos Santos Alberto <will.neto@outlook.com>
2024-09-11 20:21:55 -0300

    Laços de repetições

---
+++ src/my/javaoverview/loops/ForLoop.java
@@ -0,0 +1,11 @@
+package my.javaoverview.loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("Estruturas de Repetição - For");
+
+        for (int count = 0; count <= 10; count++){
+            System.out.println("count = " + count);
+        }
+    }
+}
---
+++ src/my/javaoverview/loops/WhileLoop.java
@@ -0,0 +1,21 @@
+package my.javaoverview.loops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        System.out.println("Estruturas de Repetição - While / Do-While");
+
+        System.out.println("\n==While (confere a condição antes de executar)== ");
+        int count = 0;
+        while (count < 10) {
+            count++;
+            System.out.println("count = " + count);
+        }
+
+        System.out.println("\n==Do-While (sempre executa uma vez antes de conferir a condição)==");
+        count = 0;
+        do {
+            System.out.println("count = " + count);
+            count++;
+        } while (count < 10);
+    }
+}
