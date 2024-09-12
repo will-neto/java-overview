6c9a4bc78c789b04e164d4a12f69895f3db52f97
Wilson dos Santos Alberto <will.neto@outlook.com>
2024-09-11 21:06:27 -0300

    Continue/Break for loop

--- src/my/javaoverview/loops/ForLoop.java
+++ src/my/javaoverview/loops/ForLoop.java
@@ -7,5 +7,25 @@ public class ForLoop {
         for (int count = 0; count <= 10; count++){
             System.out.println("count = " + count);
         }
+
+        System.out.println("\nEstruturas de Repetição - For com Break");
+
+        for (int count = 0; count <= 10; count++){
+
+            if (count == 7)
+                break;
+
+            System.out.println("count = " + count);
+        }
+
+        System.out.println("\nEstruturas de Repetição - For com Continue");
+
+        for (int count = 0; count <= 10; count++){
+
+            if (count % 2 == 1)
+                continue;
+
+            System.out.println("count = " + count + " é par");
+        }
     }
 }
