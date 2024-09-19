aebb5bb550daf669da20ec1c98fbcb45c22466d8
Wilson dos Santos Alberto <will.neto@outlook.com>
2024-09-19 20:05:52 -0300

    Arrays

---
+++ src/my/javaoverview/lists/ArraysPrimitiveTypes.java
@@ -0,0 +1,67 @@
+package my.javaoverview.lists;
+
+public class ArraysPrimitiveTypes {
+    public static void main(String[] args) {
+
+        System.out.println("== Arrays com tipos primitivos ==");
+
+        // Array de inteiros com tamanho fixo
+        int[] idades = new int[10];
+        idades[0] = 100;
+        idades[1] = 200;
+        idades[3] = 300;
+
+        System.out.println("\nPrimeiro item do objeto idades = " +  idades[0]);
+
+        System.out.println("\nExecutando idades");
+        loopArray(idades);
+
+
+        System.out.println("\nExecutando idades");
+
+        idades = new int[10]; // Criando nova instancia, alterando o tamanho
+        loopArray(idades);
+
+        System.out.println("\nUso do Vargs apenas pra evidência da utilização");
+        loopArray(1, 2, 3);
+
+        System.out.println("\nUso de outros tipos primitivos como array");
+        boolean[] arrayBool = new boolean[3];
+        float[] arrayFloat = new float[3];
+        char[] arrayChar = new char[3];
+
+        loopArray(arrayBool);
+        loopArray(arrayFloat);
+        loopArray(arrayChar);
+    }
+
+    // Uso de Vargs - int... (nada mais que uma lista de parametros que podem ser passados separados por virgula no argumento
+    // Itens devem ser do tipo int
+    public static void loopArray(int... array){
+        int count;
+        for(count = 0; count < array.length; count++){
+            System.out.println(array[count]);
+        }
+    }
+
+    public static void loopArray(boolean... array){
+        int count;
+        for(count = 0; count < array.length; count++){
+            System.out.println(array[count]);
+        }
+    }
+
+    public static void loopArray(float... array){
+        int count;
+        for(count = 0; count < array.length; count++){
+            System.out.println(array[count]);
+        }
+    }
+
+    public static void loopArray(char... array){
+        int count;
+        for(count = 0; count < array.length; count++){
+            System.out.println(array[count]);
+        }
+    }
+}
---
+++ src/my/javaoverview/lists/ArraysReferenceTypes.java
@@ -0,0 +1,44 @@
+package my.javaoverview.lists;
+
+public class ArraysReferenceTypes {
+    public static void main(String[] args) {
+
+        System.out.println("== Arrays com tipos de referênciaa ==");
+
+        // Array de inteiros com tamanho fixo
+        Integer[] idades = new Integer[10];
+        idades[0] = 100;
+        idades[1] = 200;
+        idades[3] = 300;
+
+        System.out.println("\nPrimeiro item do objeto idades = " +  idades[0]);
+
+        System.out.println("\nExecutando idades");
+        loopArray(idades);
+
+
+        System.out.println("\nExecutando idades");
+
+        idades = new Integer[10]; // Criando nova instancia, alterando o tamanho
+        loopArray(idades);
+
+
+        System.out.println("\nUso de outros tipos primitivos como array");
+        Boolean[] arrayBool = new Boolean[3];
+        Float[] arrayFloat = new Float[3];
+        Character[] arrayChar = new Character[3];
+
+        loopArray(arrayBool);
+        loopArray(arrayFloat);
+        loopArray(arrayChar);
+    }
+
+    // Uso de Object
+    // Classe pai de todos os tipos referências
+    public static void loopArray(Object[] array){
+        int count;
+        for(count = 0; count < array.length; count++){
+            System.out.println(array[count]);
+        }
+    }
+}
